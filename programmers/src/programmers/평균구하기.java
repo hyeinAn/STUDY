2d31011347ebe6a3f4bfb0d67c19a1f17a138506
hyeinAn <ahi1346@naver.com>
2021-10-21 17:40:40 +0900

    12944

---
+++ programmers/src/programmers/평균구하기.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Æò±Õ±¸ÇÏ±â {
+
+	public static double main(int[] arr) {
+		double answer = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			answer += arr[i];
+		}
+		return answer/arr.length;
+	}
+
+}
